17254a06edfbafb10aaea8900e1bb439b5af9791
preetam-prajapati <95955622+preetam-prajapati@users.noreply.github.com>
2022-04-09 12:32:46 +0530

    redux tut

---
+++ containers/HomeContainer.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import Home from '../components/Home'
+import {addToCart,removeToCart} from '../Service/Actions/actions'
+
+
+const mapStateToProps=state =>({
+    cardData:state
+})
+const mapDispatchToProps= dispatch =>({
+    addToCartHandler:data=>dispatch(addToCart(data)),
+    removeToCartHandler:data=>dispatch(removeToCart(data))
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(Home)
+
